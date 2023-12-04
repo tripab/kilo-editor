f00cfddcf9d45fdcefa1ab9ee2d4c0659df208ad
Varsha Tiwari <varsha@Varshas-MacBook-Air.local>
2023-12-04 15:46:51 +0530

    chapter 7: colourful search results

--- kilo.c
+++ kilo.c
@@ -39,7 +39,8 @@ enum editorKey {
 
 enum editorHighlight {
     HL_NORMAL = 0,
-    HL_NUMBER
+    HL_NUMBER,
+    HL_MATCH
 };
 
 /*** data ***/
@@ -222,6 +223,8 @@ int editorSyntaxToColour(int hl) {
     switch (hl) {
         case HL_NUMBER:
             return 31;
+        case HL_MATCH:
+            return 34;
         default:
             return 37;
     }
@@ -490,6 +493,8 @@ void editorFindCallback(char *query, int key) {
             E.cy = current;
             E.cx = editorRowRxToCx(row, match - row->render);
             E.rowoff = E.numrows;
+
+            memset(&row->hl[match - row->render], HL_MATCH, strlen(query));
             break;
         }
     }
